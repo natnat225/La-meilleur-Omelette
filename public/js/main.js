8f454a9e264a563afe8d6a6537378122e91267c1
Nat nat <natnat@MacBook-Air-de-Nat.local>
2023-11-21 10:39:17 +0100

    add

---
+++ public/js/main.js
@@ -0,0 +1,43 @@
+/* Créer un objet personne. Cette personne doit avoir des propriétés et des méthodes : 
+nom(string)
+lieu(string)
+argent(number)
+mainDroite(tableau) du coup main gauche(tableau)
+seDeplacer(lieu)
+payerArticle(article)
+couper(ingredient, outil) */
+let personne = {
+    nom: "Nathan",
+    lieu: "",
+    argent: 100,
+    mainDroite: [],
+    mainGauche: [],
+  
+    seDeplacer(lieu) {
+  
+    },
+  
+    payerArticle(article) {
+
+    },
+  
+    couper(ingredient, outil) {
+
+    },
+}
+  
+  
+  // Création de l'objet "maison"
+  let maison = {
+    nom: "maison",
+    personnes: [],
+  };
+  
+  // Création de l'outil "couteau"
+  let couteau = {
+    nom: "couteau",
+    action: "coupe",
+  };
+
+
+  
